a20b2672229ea0294d2315b8e60f82731526d3bb
wongwander <490624198@qq.com>
2014-11-06 17:50:15 +0800

    Initial Commit

---
+++ TEST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST
+//
+//  Created by Deidara on 14/11/6.
+//  Copyright (c) 2014年 deidara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
